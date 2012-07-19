5049208c509324414f038f24e19d469b4ec91e66
DimitarChristoff <christoff@gmail.com>
2012-07-19 13:25:19 +0100

    moved to node.js doc generation

--- generate-docs.sh
+++
@@ -1,7 +0,0 @@
-git reset --hard HEAD 
-curl --silent http://documentup.com/DimitarChristoff/Epitome/recompile
-curl --silent --output index.html http://documentup.com/DimitarChristoff/Epitome/
-git add index.html
-git commit -m'updating docs'
-git push
-open http://dimitarchristoff.github.com/Epitome
