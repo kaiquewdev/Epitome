97f9768127f61dfaf2bce7450db51b1d1b5449f5
DimitarChristoff <christoff@gmail.com>
2012-07-23 15:10:36 +0100

    styled docs

--- js/docs.js
+++ js/docs.js
@@ -1,7 +1,11 @@
 window.addEvent('domready', function() {
 	new moostrapScrollspy('sections', {
 		onReady: function() {
-			this.scroll();
+			//  this.scroll();
+		},
+		onActive: function(el) {
+			this.element.getElements('li.active').removeClass('active');
+			el.getParents("li").getLast().addClass('active');
 		}
 	});
 });
