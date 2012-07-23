7649beb9f193c0b815f3b1131fa1403d0e5a29ee
DimitarChristoff <christoff@gmail.com>
2012-07-23 15:38:02 +0100

    moved to node request instead of http

--- build.js
+++ build.js
@@ -1,76 +1,64 @@
 #!/usr/bin/env node
 var dom = require('jsdom'),
-	request = require('http'),
+	request = require('request'),
 	fs = require('fs'),
 	repo = 'DimitarChristoff/Epitome',
-	build = {
-		host: 'documentup.com',
-		path: repo + '?theme=v1'
-	},
-	compile = {
-		host: 'documentup.com',
-		path: repo + '/recompile'
-	},
+	build = 'http://documentup.com/' + repo,
+	compile = 'http://documentup.com/' + repo + '/recompile/',
 	writeDocs = function() {
-		request.get(build).on('response', function(response) {
-			var html = '';
+		request(build, function(error, response, html) {
+			if (error) {
+				console.log('Failed to get documentation.', error);
+				process.exit(1);
+			}
 
-			// read the data
-			response.setEncoding('utf-8');
-			response.on('data', function(chunk) {
-				html += chunk;
-			});
-			response.on('end', function() {
-				// now, create a jsdom document out of the response, injecting the extra scripts
-				dom.env(html, [
-					'http://ajax.googleapis.com/ajax/libs/mootools/1.4.5/mootools-yui-compressed.js',
-					'js/moostrap-scrollspy.js',
-					'js/docs.js'
-				],
-				function(errors, window) {
-					var head = window.getDocument().getElement('head'),
-						css = head.getElement('link');
+			// now, create a jsdom document out of the response, injecting the extra scripts
+			dom.env(html, [
+				'http://ajax.googleapis.com/ajax/libs/mootools/1.4.5/mootools-yui-compressed.js',
+				'js/moostrap-scrollspy.js',
+				'js/docs.js'
+			],
+			function(errors, window) {
+				var head = window.getDocument().getElement('head'),
+					css = head.getElement('link');
 
 
 
-					// add custom stylesheet
-					new window.Element('link', {
-						href: 'css/docs.css',
-						type: 'text/css',
-						rel: 'stylesheet'
-					}).replaces(css);
+				// add custom stylesheet
+				new window.Element('link', {
+					href: 'css/docs.css',
+					type: 'text/css',
+					rel: 'stylesheet'
+				}).replaces(css);
 
-					// move the scripts to the head
-					window.document.getElements('.jsdom').removeClass('jsdom').inject(head);
+				// move the scripts to the head
+				window.document.getElements('.jsdom').removeClass('jsdom').inject(head);
 
-					// fix doctype
-					html = ['<!DOCTYPE html>', window.document.innerHTML].join('\n');
+				// fix doctype
+				html = ['<!DOCTYPE html>', window.document.innerHTML].join('\n');
 
-					// write to file.
-					fs.writeFile('index.html', html, function(error) {
-						if (error) {
-							console.log('Failed to create index.html. ', error);
-							process.exit(1);
-						}
-						else {
-							console.log('index.html was created');
-							process.exit(0);
-						}
-					});
+				// write to file.
+				fs.writeFile('index.html', html, function(error) {
+					if (error) {
+						console.log('Failed to create index.html. ', error);
+						process.exit(1);
+					}
+					else {
+						console.log('index.html was created');
+						process.exit(0);
+					}
 				});
 			})
 
-		}).on('error', function() {
-			console.log('Failed to get documentation.');
-			process.exit(1);
 		});
 	};
 
 // get going. compile first, then get the new docs.
-request.get(compile, function() {
+request(compile, function(error, response, html) {
+	if (error) {
+		console.log('Failed to recompile docs.', error);
+		process.exit(1);
+	}
 	// give it some time to generate
 	setTimeout(writeDocs, 2000);
-}).on('error', function() {
-	console.log('Failed to recompile docs.');
-	process.exit(1);
 });
